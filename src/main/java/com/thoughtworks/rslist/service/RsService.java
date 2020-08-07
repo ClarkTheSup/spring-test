3b795d5ce5c1db6db2eb8fa2181597e80d75528c
ClarkTheSup <lknkoqq123@163.com>
2020-08-07 16:31:14 +0800

    buy rs to generate trade

--- src/main/java/com/thoughtworks/rslist/service/RsService.java
+++ src/main/java/com/thoughtworks/rslist/service/RsService.java
@@ -3,9 +3,11 @@ package com.thoughtworks.rslist.service;
 import com.thoughtworks.rslist.domain.Trade;
 import com.thoughtworks.rslist.domain.Vote;
 import com.thoughtworks.rslist.dto.RsEventDto;
+import com.thoughtworks.rslist.dto.TradeDto;
 import com.thoughtworks.rslist.dto.UserDto;
 import com.thoughtworks.rslist.dto.VoteDto;
 import com.thoughtworks.rslist.repository.RsEventRepository;
+import com.thoughtworks.rslist.repository.TradeRepository;
 import com.thoughtworks.rslist.repository.UserRepository;
 import com.thoughtworks.rslist.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,14 @@ public class RsService {
   final RsEventRepository rsEventRepository;
   final UserRepository userRepository;
   final VoteRepository voteRepository;
+  final TradeRepository tradeRepository;
 
-  public RsService(RsEventRepository rsEventRepository, UserRepository userRepository, VoteRepository voteRepository) {
+  public RsService(RsEventRepository rsEventRepository, UserRepository userRepository
+          , VoteRepository voteRepository, TradeRepository tradeRepository) {
     this.rsEventRepository = rsEventRepository;
     this.userRepository = userRepository;
     this.voteRepository = voteRepository;
+    this.tradeRepository = tradeRepository;
   }
 
   public void vote(Vote vote, int rsEventId) {
@@ -50,6 +55,13 @@ public class RsService {
   }
 
   public void buy(Trade trade, int id) {
-
+    RsEventDto rsEventDto = rsEventRepository.findById(id).orElse(null);
+    if (rsEventDto == null) {
+      throw new RuntimeException();
+    }
+    TradeDto tradeDto = TradeDto.builder()
+            .amount(trade.getAmount()).ranking(trade.getRank())
+            .rs_event_tdo(rsEventDto).build();
+    tradeRepository.save(tradeDto);
   }
 }
--- src/test/java/com/thoughtworks/rslist/service/RsServiceTest.java
+++ src/test/java/com/thoughtworks/rslist/service/RsServiceTest.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.rslist.service;
 
+import com.thoughtworks.rslist.domain.Trade;
 import com.thoughtworks.rslist.domain.Vote;
 import com.thoughtworks.rslist.dto.RsEventDto;
+import com.thoughtworks.rslist.dto.TradeDto;
 import com.thoughtworks.rslist.dto.UserDto;
 import com.thoughtworks.rslist.dto.VoteDto;
 import com.thoughtworks.rslist.repository.RsEventRepository;
+import com.thoughtworks.rslist.repository.TradeRepository;
 import com.thoughtworks.rslist.repository.UserRepository;
 import com.thoughtworks.rslist.repository.VoteRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,13 +29,15 @@ class RsServiceTest {
   @Mock RsEventRepository rsEventRepository;
   @Mock UserRepository userRepository;
   @Mock VoteRepository voteRepository;
+  @Mock
+  TradeRepository tradeRepository;
   LocalDateTime localDateTime;
   Vote vote;
 
   @BeforeEach
   void setUp() {
     initMocks(this);
-    rsService = new RsService(rsEventRepository, userRepository, voteRepository);
+    rsService = new RsService(rsEventRepository, userRepository, voteRepository, tradeRepository);
     localDateTime = LocalDateTime.now();
     vote = Vote.builder().voteNum(2).rsEventId(1).time(localDateTime).userId(1).build();
   }
@@ -88,5 +93,27 @@ class RsServiceTest {
         () -> {
           rsService.vote(vote, 1);
         });
+  }
+
+  @Test
+  void when_buy_rs_and_no_ranking_bought_then_add_trade() {
+    UserDto userDto = UserDto.builder().voteNum(5).phone("18888888888").gender("female")
+                    .email("a@b.com").age(19).userName("xiaoli").id(2).build();
+    RsEventDto rsEventDto = RsEventDto.builder().eventName("event name").id(1)
+                    .keyword("keyword").voteNum(2).user(userDto).build();
+    TradeDto tradeDto = TradeDto.builder().amount(100).ranking(1)
+            .rs_event_tdo(rsEventDto).build();
+    when(userRepository.findById(anyInt())).thenReturn(Optional.of(userDto));
+    when(rsEventRepository.findById(anyInt())).thenReturn(Optional.of(rsEventDto));
+    when(tradeRepository.findById(anyInt())).thenReturn(Optional.of(tradeDto));
+
+    Trade trade = Trade.builder().amount(tradeDto.getAmount())
+            .rank(tradeDto.getRanking()).build();
+
+    userRepository.save(userDto);
+    rsEventRepository.save(rsEventDto);
+    rsService.buy(trade, rsEventDto.getId());
+
+    verify(tradeRepository).save(tradeDto);
   }
 }
